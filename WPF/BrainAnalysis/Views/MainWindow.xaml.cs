90930b9ec08e23ea8074452c63f4a97e3326cee8
zhe.li03_HSW-GS <zhe.li03@united-imaging.com>
2020-09-08 18:18:29 +0800

    Update

---
+++ WPF/BrainAnalysis/Views/MainWindow.xaml.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrainAnalysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Page and Logo button function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Page_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Logo_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// common tool button function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Select_Button_Click(object sender, RoutedEventArgs e) 
+        {
+            if (MessageBox.Show("Select_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Zoom_In_Out_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Zoom_In_Out_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+        
+        private void Pan_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Pan_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Whirl_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Whirl_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+        
+
+        private void Fit_Window_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Fit_Window_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Default_Window_Width_Height_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Default_Window_Width_Height_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+        private void Pixel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Pixel_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Draw_Circle_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Draw_Circle_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Measure_Angle_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Measure_Angle_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Measure_Line_Distance_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Measure_Line_Distance_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Add_Arrow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Add_Arrow_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void Delete_All_Roi_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Delete_All_Roi_Button_Click", "Status", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                ;
+            }
+            else
+            {
+                ;
+
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPF/LinkSettingDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Xml;
+
+namespace LinkSettingDemo
+{
+    
+    public class MainWindowViewModel : AppViewModelBase
+    {
+        public List<CheckBoxViewModel> CheckBoxList { get; set; }
+        public List<CheckBoxViewModel> CopyCheckBoxList { get; set; }
+
+        private bool isAllChecked;
+        public bool IsAllChecked
+        {
+            get { return isAllChecked; }
+            set 
+            { 
+                isAllChecked = value;
+                foreach (var item in CheckBoxList)
+                {
+                    if (isAllChecked)
+                    {
+                        item.IsChecked = true;
+                    }
+                    else
+                    {
+                        item.IsChecked = false;
+                    }
+                    
+                }
+                RaisePropertyChanged("IsAllChecked");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            //deser xml
+            string xmlPath = "D:/Project_Code/WPF/LinkSettingDemo/LinkSettingList/LinkSettingList.xml";
+            CheckBoxList = GetCheckBoxListFromXML(xmlPath);
+            CopyCheckBoxList = GetCheckBoxListFromXML(xmlPath);
+        }
+        #region GetCheckBoxListFromXML
+        public List<CheckBoxViewModel> GetCheckBoxListFromXML(string xmlPath)
+        {
+            
+            try
+            {
+                var allCheckBoxList = new List<CheckBoxViewModel>();
+                var xmlDoc = new XmlDocument();
+                xmlDoc.Load(xmlPath);
+                //xmlDoc.DocumentElement->parse to Root
+                var checkBoxItems = xmlDoc.DocumentElement.ChildNodes;
+                for (int j = 0; j < checkBoxItems.Count; ++j)
+                {
+                    var name = checkBoxItems[j].Attributes["Name"].Value;
+                    var isChecked = checkBoxItems[j].Attributes["IsChecked"].Value;
+                    var checkBoxItem = new CheckBoxViewModel
+                    {
+                        MainWindowViewModel = this,
+                        Name = name,
+                        IsChecked = Convert.ToBoolean(isChecked),
+                    };
+                    allCheckBoxList.Add(checkBoxItem);
+                }
+                return allCheckBoxList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error:" + ex.ToString());
+                return null;
+            }
+        }
+        #endregion
+
+        #region GetCheckBoxListSelectedCount
+        public int GetCheckBoxListSelectedCount()
+        {
+            int count = 0;
+            foreach(var item in CheckBoxList)
+            {
+                if(item.IsChecked == true)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+        #endregion
+
+        #region SelectAll
+        public ICommand SelectAllCommand => new DelegateCommand(SelectAll);
+        
+        private void SelectAll(object commandParameter)
+        {
+            foreach (var item in CheckBoxList)
+            {
+                item.IsChecked = true;
+            }
+            IsAllChecked = true;
+        }
+        #endregion
+
+        #region SelectNone
+        public ICommand SelectNoneCommand => new DelegateCommand(SelectNone);
+        private void SelectNone(object commandParameter)
+        {
+            foreach (var item in CheckBoxList)
+            {
+                item.IsChecked = false;
+            }
+            IsAllChecked = false;
+        }
+
+
+        #endregion
+
+        #region SelectReverse
+        public ICommand SelectReverseCommand => new DelegateCommand(SelectReverse);
+        private void SelectReverse(object commandParameter)
+        {
+            foreach (var item in CheckBoxList)
+            {
+                if (item.IsChecked == true)
+                {
+                    item.IsChecked = false;
+                }
+                else if (item.IsChecked == false)
+                {
+                    item.IsChecked = true;
+                }
+            }
+        }
+        #endregion
+
+        #region SaveConfig
+        public ICommand SaveConfigCommand => new DelegateCommand(SaveConfig);
+        private void SaveConfig(object commandParameter)
+        {
+            var xmlDoc = new XmlDocument();
+            XmlNode node = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", "no");
+            xmlDoc.AppendChild(node);
+
+            XmlElement root = xmlDoc.CreateElement("Root");
+            xmlDoc.AppendChild(root);
+
+            foreach(var item in CheckBoxList)
+            {
+                XmlElement itemElement = xmlDoc.CreateElement("Item");
+                itemElement.SetAttribute("Name", item.Name);
+                itemElement.SetAttribute("IsChecked", item.IsChecked.ToString());
+                root.AppendChild(itemElement);
+            }
+            xmlDoc.Save(@"D:\Project_Code\WPF\LinkSettingDemo\LinkSettingList\LinkSettingList.xml");
+            MessageBox.Show("当前状态成功写入，可打开LinkSettingList.xml查看");
+        }
+        #endregion
+
+        #region Refresh
+        public ICommand RefreshCommand => new DelegateCommand(Refresh);
+        private void Refresh(object commandParameter)
+        {
+            
+        }
+        #endregion
+
+        #region Cancel
+        public ICommand CancelCommand => new DelegateCommand(Cancel);
+        private void Cancel(object commandParameter)
+        {
+            //save CopyCheckBoxList to file
+            var xmlDoc = new XmlDocument();
+            XmlNode node = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", "no");
+            xmlDoc.AppendChild(node);
+
+            XmlElement root = xmlDoc.CreateElement("Root");
+            xmlDoc.AppendChild(root);
+
+            foreach (var item in CopyCheckBoxList)
+            {
+                XmlElement itemElement = xmlDoc.CreateElement("Item");
+                itemElement.SetAttribute("Name", item.Name);
+                itemElement.SetAttribute("IsChecked", item.IsChecked.ToString());
+                root.AppendChild(itemElement);
+            }
+            xmlDoc.Save(@"D:\Project_Code\WPF\LinkSettingDemo\LinkSettingList\LinkSettingList.xml");
+            MessageBox.Show("取消写入，恢复初始配置数据，可打开LinkSettingList.xml查看");
+        }
+        #endregion
+
+        #region GetParentObject - not used func
+        public T GetParentObject<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+
+            while (parent != null)
+            {
+                if (parent is T && (((T)parent).Name == name | string.IsNullOrEmpty(name)))
+                {
+                    return (T)parent;
+                }
+
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return null;
+        }
+        #endregion
+
+        #region FindVisualChild - not used func
+        List<T> FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            try
+            {
+                List<T> list = new List<T>();
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                    if (child is T)
+                    {
+                        list.Add((T)child);
+                        List<T> childOfChildren = FindVisualChild<T>(child);
+                        if (childOfChildren != null)
+                        {
+                            list.AddRange(childOfChildren);
+                        }
+                    }
+                    else
+                    {
+                        List<T> childOfChildren = FindVisualChild<T>(child);
+                        if (childOfChildren != null)
+                        {
+                            list.AddRange(childOfChildren);
+                        }
+                    }
+                }
+
+                return list;
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show(ee.Message);
+                return null;
+            }
+        }
+        #endregion
+    }
+}
